e86d7709c3ee612e25b478195d0a89f68bfc97ed
sidiktsq <baika3542@gmail.com>
2026-04-24 11:33:41 +0700

    .

--- scratch/inspect_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-use Filament\Schemas\Schema;
-
-$reflection = new ReflectionClass(Schema::class);
-$methods = array_map(fn($m) => $m->name, $reflection->getMethods());
-
-echo implode("\n", $methods);
